76458c29e7857e6673f61aaff89064541927dd06
Mae80 <maisjam@gmail.com>
2018-08-07 18:23:27 +0200

    added changes to slick carousel

---
+++ resources/js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    slideToShow: 1,
+    slideToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    accessibility: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  });
+});
